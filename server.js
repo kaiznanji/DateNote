990f8fe222da151b0f007479bd95f644acebed04
Lakshya Balchandani <lakshyabalchandani@v1041-wn-rt-d-161-54.campus-dynamic.uwaterloo.ca>
2021-03-06 20:56:44 -0500

    Lakshya's Frontend V1

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const notes = require('./routes/notes')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use('/notes', notes)
+
+app.get('/', (req, res) => {
+    res.render('main')
+})
+
+
+app.listen(5000)
